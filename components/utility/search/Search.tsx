b8bb434edd6eb28af3a1240cb9b96b5513dbe003
Farya Bakhsh <faryabakhsh@gmail.com>
2022-08-22 21:47:39 +0500

    feat: updated serach component

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,11 +1,17 @@
-export interface ISearch {
-  sampleTextProp: string;
-}
+export interface ISearch {}
 
-const Search: React.FC<ISearch> = ({ sampleTextProp }) => {
+const Search: React.FC<ISearch> = () => {
   return (
-    <form>
-      <input type="text" />
+    <form
+      className="flex flex-col items-center gap-y-5"
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <input
+        className="rounded-full border-2 w-5/6 sm:w-128 h-12 px-3"
+        type="text"
+      />
       <button type="submit">Google Search</button>
       <button type="submit">I&apos;m Feeling Lucky </button>
     </form>
